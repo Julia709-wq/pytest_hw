be9f6d434fc35d46ead2b56698aa4695b731a114
Julia <yulia.2018.ivanova@gmail.com>
2024-10-10 02:30:14 +0300

    parametrize added

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,11 @@
 def get_mask_card_number(card_number: int):
-    return (str(card_number)[:4] + " " + str(card_number)[4:6] + "** **** "
-            + str(card_number)[12:])
+    if len(str(card_number)) != 16:
+        return '*' * len(str(card_number))
+    elif type(card_number) != int:
+        return None
+    else:
+        return (str(card_number)[:4] + " " + str(card_number)[4:6] + "** **** "
+                + str(card_number)[12:])
 
 
 def get_mask_account(account_number: int):
