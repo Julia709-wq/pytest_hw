39d8a591b2feeb3f85e2f7fc1f458ba1ad86c76a
Julia <yulia.2018.ivanova@gmail.com>
2024-10-10 01:45:17 +0300

    fixtures added

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,6 +2,16 @@ from src.masks import get_mask_card_number, get_mask_account
 import pytest
 
 
+@pytest.fixture
+def card_number():
+    return 7000792289606361
+
+
+@pytest.fixture
+def account_number():
+    return 73654108430135874305
+
+
 def test_get_mask_card_number():
     assert get_mask_card_number(7000792289606361) == '7000 79** **** 6361'
 
